690bc69b0985d4da8072b31f20dba0ae39e0cf4f
Dennis Ly <dennisly1995@gmail.com>
2019-05-02 15:46:34 -0700

    Se comienza a trabajar en la primera tarjeta.

--- tasksgenerator/src/App.js
+++ tasksgenerator/src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from "./components/Navigation";
+import todos from "./todos.json";
 
-function App() {
-  return (
-    <div className="App">
-    <Navigation titulo="Tasks Generator"/>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = todos;
+  }
+
+
+  render() {
+
+    
+
+    return (
+      <div className="App">
+        <Navigation titulo="Tasks Generator" />
         <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
 export default App;
