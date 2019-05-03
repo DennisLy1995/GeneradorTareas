654209368ee0b075ca67a8c248eeb0d96e2386b3
Dennis Ly <dennisly1995@gmail.com>
2019-05-03 15:00:36 -0700

    Validations in the card component.

--- tasksgenerator/src/Card.js
+++ tasksgenerator/src/Card.js
@@ -12,30 +12,31 @@ class Card extends React.Component {
   render() {
 
     return (
-      <div className="card border-secondary mb-3" style={{ minWidth: '15rem', width:'20rem', margin: '1%' , display: 'inline-block'}}>
+      <div className="card border-secondary mb-3" style={{ minWidth: '15rem', width: '20rem', margin: '1%', display: 'inline-block' }}>
 
-        <div className="card-header"  style={{textAlign:'center'}}>
+        <div className="card-header" style={{ textAlign: 'center' }}>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Title</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Add your title" />
+            <input type="email" className="form-control" id="titleNew" aria-describedby="emailHelp" placeholder="Add your title" />
           </div>
         </div>
 
-        <div className="card-body" style={{textAlign:'center'}}>
+        <div className="card-body" style={{ textAlign: 'center' }}>
           <div className="form-group">
             <label htmlFor="description">Description</label>
-            <textarea className="form-control" id="description" rows="3"></textarea>
+            <textarea className="form-control" id="descriptionNew" rows="3"></textarea>
           </div>
           <small id="emailHelp" className="form-text text-muted">Keep your tasks in order...</small>
         </div>
-        
+
         <div className="card-footer">
           <button type="button" className="btn btn-info" onClick={addInformationalCard}>Add task</button>
+
+          <div style={{ float: 'right', marginBottom: '10px' }}>
           
-          <div style={{float:'right', marginBottom:'10px'}}>
             <div>
               <label className="form-check-label">
-                <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios1" value="Low"/>
+                <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios1" value="Low" />
                 Low </label>
             </div>
 
@@ -45,7 +46,6 @@ class Card extends React.Component {
                 Medium </label>
             </div>
 
-
             <div>
               <label className="form-check-label">
                 <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios3" value="High" />
@@ -54,26 +54,47 @@ class Card extends React.Component {
 
           </div>
         </div>
-
-
       </div>
-
-
-
     );
   }
 }
 
 export default Card;
 
-function addInformationalCard(){
+function addInformationalCard() {
 
-  var title = "title test";
-  var description = "Description test";
-  var priority  = "High";
-  ReactDOM.render(<InformationCard title={title} description={description} priority={priority}/>, document.getElementById('tasksContainer'));
-  
+  var title = document.getElementById('titleNew').value;
+  var description = document.getElementById('descriptionNew').value;
+  var priority = '';
+  var radio1 = document.getElementById('optionsRadios1');
+  var radio2 = document.getElementById('optionsRadios2');
+  var radio3 = document.getElementById('optionsRadios3');
 
+  if (radio1.checked) {
+    priority = 'High';
+  } else if (radio2.checked) {
+    priority = 'Medium';
+  } else if (radio3.checked) {
+    priority = 'Low';
+  } else {
+    priority = '';
+  }
+
+  if (title == "" || description == "" || priority == "") {
+    alert('Complete all the fills in the card');
+  } else {
+    ReactDOM.render(<InformationCard title={title} description={description} priority={priority} />, document.getElementById('tasksContainer'));
+    clearMainCard();
+  }
+
+}
+
+function clearMainCard() {
+  document.getElementById('titleNew').value = '';
+  document.getElementById('descriptionNew').value = '';
+  document.getElementById('optionsRadios1').checked = false;
+  document.getElementById('optionsRadios2').checked = false;
+  document.getElementById('optionsRadios3').checked = false;
 }
 
 
