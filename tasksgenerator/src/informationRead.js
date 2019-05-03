9198c927644ab016a46ba0bd431bc673566c14c7
Dennis Ly <dennisly1995@gmail.com>
2019-05-03 12:38:03 -0700

    Continue workingo on the add of the component task dinamically.

--- tasksgenerator/src/informationRead.js
+++ tasksgenerator/src/informationRead.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { listenerCount } from "events";
+import logo from './list.svg';
 
 class informationBlock extends React.Component {
     constructor() {
@@ -8,7 +10,7 @@ class informationBlock extends React.Component {
         return (
 
             <div style={{width: '67%', display: 'inline-block', padding: '2%', color:'white', textAlign:'center', marginTop:'5%', position:'absolute'}}>
-                <h2>{this.props.title}</h2>
+                <h2>{this.props.title}<img src={logo} className="App-logo-task" alt="logo" style={{marginLeft:'2%'}}/></h2>
                 <p></p>
                 <p>{this.props.information}</p>
             </div>
